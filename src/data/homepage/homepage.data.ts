12b0e7f60f39bb816d06a5ab4f3327d3fe9c1e2d
Vincent Battez <vincent.battez.pro@gmail.com>
2022-02-23 02:20:56 -0500

    update text

--- src/data/homepage/homepage.data.ts
+++ src/data/homepage/homepage.data.ts
@@ -1,9 +1,9 @@
 import faceVector from '../../assets/face-vector.svg';
 
 export const homepageData = {
-  subTitle: 'Porfolio Développeur Front-End Confirmé',
-  title: 'Développeur Vue.js Confirmé affinité UX/UI',
-  description: 'Je peux lead un projet Vue.js.',
+  subTitle: 'Porfolio Développeur Front-End Freelance',
+  title: 'Développeur Vue.js Freelance',
+  description: 'Disponible dès maintenant pour créer ou architecturer un projet Vue.js avec passion',
   image: {
     url: faceVector,
   },
