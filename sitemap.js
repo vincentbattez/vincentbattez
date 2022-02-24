ecace9c2f35da0530742e821828442eff6024c75
Vincent Battez <vincent.battez.pro@gmail.com>
2022-02-24 00:34:34 -0500

    sitemap

--- sitemap.js
+++ sitemap.js
@@ -3,7 +3,7 @@ const SitemapGenerator = require('sitemap-generator')
 // create generator
 const generator = SitemapGenerator('https://vincentbattez.dev/', {
   stripQuerystring: false,
-  filepath: './sitemap.xml',
+  filepath: './public/sitemap.xml',
 });
 
 // start the crawler
